4a52746dc7073ba3bb623ead0b8ba7089c0aa336
kurshakuz <abilkasov@gmail.com>
2023-02-18 15:16:13 +0000

    add setup scripts

---
+++ ego4d/setup.sh
@@ -0,0 +1,14 @@
+export AWS_ACCESS_KEY_ID="AKIATEEVKTGZNURSIMNK"
+export AWS_SECRET_ACCESS_KEY="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"
+
+# Set up the AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -o awscliv2.zip >/dev/null
+sudo ./aws/install >/dev/null 2>&1
+aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID" && aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
+rm "awscliv2.zip"
+
+# Download the Ego4D Annotations to ego4d_data/
+ego4d --output_directory="/workspaces/content/ego4d_data/" --datasets annotations sta_models omnivore_video_swinl_fp16 --benchmarks FHO -y
+
+# ls /workspaces/content/ego4d_data/v1/annotations | grep fho
