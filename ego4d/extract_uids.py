06eea855c528ba813fd4108ec18b05d0e12251b2
kurshakuz <abilkasov@gmail.com>
2023-03-14 09:14:31 +0000

    update parser

--- ego4d/extract_uids.py
+++ ego4d/extract_uids.py
@@ -4,6 +4,7 @@ import json
 data = json.load(open("../content/ego4d_data/v1/annotations/fho_hands_train.json"))
 
 # extract objects that contain "contact_frame" instance
+uids = set()
 fho_data = {
     "version": data["version"],
     "date": data["date"],
@@ -13,19 +14,27 @@ fho_data = {
     "clips": [],
 }
 for clip in data["clips"]:
-    new_clip = {
-        "clip_id": clip["clip_id"],
-        "clip_uid": clip["clip_uid"],
-        "video_uid": clip["video_uid"],
-        "frames": [],
-    }
     for frame in clip["frames"]:
         if "contact_frame" in frame:
+            new_clip = {
+                "clip_id": clip["clip_id"],
+                "clip_uid": clip["clip_uid"],
+                "video_uid": clip["video_uid"],
+                "frames": [],
+            }
             new_clip["frames"].append(frame)
+
+            uids.add(clip["video_uid"])
     fho_data["clips"].append(new_clip)
     # break
 
 # save the extracted frames
 with open("./fho_hands_train_contact.json", "w") as f:
     json.dump(fho_data, f, indent=2)
+
+# store uids string
+with open("./fho_hands_train_contact_uids.txt", "w") as f:
+    f.write(' '.join(uids))
+
+# print(uids)
 # json.dumps(contact_clips, indent=2)
