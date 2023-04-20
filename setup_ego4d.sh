62b831cbc0b76279c0b0063d56ee47c96315756c
Shyngyskhan Abilkassov <shyngyskhan.abilkassov@bmw.de>
2023-04-20 13:54:09 +0600

    add evaluation file

--- setup_ego4d.sh
+++ setup_ego4d.sh
@@ -9,25 +9,25 @@ sudo apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
 pip install gdown
 sudo apt-get install unzip -y
 gdown $DRIVE_LINK
-unzip $PWD/ego4d_data_annot.zip -d /$PWD
-rm $PWD/ego4d_data_annot.zip
+unzip /workspace/ego4d_data_annot.zip -d /workspace
+# rm $PWD/ego4d_data_annot.zip
 
 # Clone thesis repository
 test -e thesis-ws || git clone https://github.com/kurshakuz/thesis-ws.git
-cd thesis-ws/
+cd /workspace/thesis-ws/
 git pull
 
 # Install requirements
-pip install -r $PWD/thesis-ws/ego4d/forecasting/ego4d/requirements.txt
+pip install -r /workspace/thesis-ws/ego4d/forecasting/requirements.txt
 
 # Install slowfast
-cd Ego4D-Future-Hand-Prediction
+cd /workspace/thesis-ws/ego4d/forecasting/Ego4D-Future-Hand-Prediction
 python3 slowfast_setup.py build develop
 
 # Downlaod weights
-mkdir -p $PWD/thesis-ego4d/results/output
-mkdir -p $PWD/thesis-ego4d/results/checkpoints/
-wget https://dl.fbaipublicfiles.com/pyslowfast/model_zoo/kinetics400/I3D_8x8_R50.pkl -P $PWD/thesis-ego4d/results
+mkdir -p /workspace/thesis-ego4d/results/output
+mkdir -p /workspace/thesis-ego4d/results/checkpoints/
+wget https://dl.fbaipublicfiles.com/pyslowfast/model_zoo/kinetics400/I3D_8x8_R50.pkl -P /workspace/thesis-ego4d/results
 
 # Start training
-python3 thesis-ws/ego4d/forecasting/Ego4D-Future-Hand-Prediction/tools/run_net.py --cfg $PWD/thesis-ws/ego4d/forecasting/Ego4D-Future-Hand-Prediction/configs/Ego4D/I3D_8x8_R50_colab.yaml OUTPUT_DIR $PWD/thesis-ego4d/results/
+python3 /workspace/thesis-ws/ego4d/forecasting/Ego4D-Future-Hand-Prediction/tools/run_net.py --cfg /workspace/thesis-ws/ego4d/forecasting/Ego4D-Future-Hand-Prediction/configs/Ego4D/I3D_8x8_R50_vast_3090.yaml OUTPUT_DIR /workspace/thesis-ego4d/results/
