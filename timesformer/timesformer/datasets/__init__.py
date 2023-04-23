dfb7edb24756922c2fde6635939eac0a2f019b0f
kurshakuz <abilkasov@gmail.com>
2023-04-23 16:25:46 +0600

    update dataset init

--- timesformer/timesformer/datasets/__init__.py
+++ timesformer/timesformer/datasets/__init__.py
@@ -1,5 +1,3 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-
+from .ego4dhand import Ego4dhand
 from .build import DATASET_REGISTRY, build_dataset  # noqa
-from .kinetics import Kinetics  # noqa
-from .ssv2 import Ssv2  # noqa
