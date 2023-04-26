0c7c18ccd2f144c7906140dac9cd1322d9b4130d
Shyngyskhan Abilkassov <shyngyskhan.abilkassov@bmw.de>
2023-04-26 17:38:46 +0600

    add uniformerv2 repo

---
+++ uniformerv2/extract_clip/extract_clip.py
@@ -0,0 +1,11 @@
+import clip as clip
+import torch
+from collections import OrderedDict
+
+model, _ = clip.load("ViT-L/14", device='cpu')
+new_state_dict = OrderedDict()
+for k, v in model.state_dict().items():
+    if 'visual.' in k:
+        if k[7:] not in ["proj", "ln_post.weight", "ln_post.bias"]:
+            new_state_dict[k[7:]] = v
+torch.save(new_state_dict, 'vit_l14.pth')
