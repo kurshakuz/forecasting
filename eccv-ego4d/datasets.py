ec4f1faee0c06898e8335c86968b82514b26da19
kurshakuz <abilkasov@gmail.com>
2023-06-07 17:52:48 +0600

    fix dataset issue

--- eccv-ego4d/datasets.py
+++ eccv-ego4d/datasets.py
@@ -162,7 +162,7 @@ def build_dataset(is_train, test_mode, args):
             new_height=256,
             new_width=320,
             args=args)
-        verb_classes = 20
+        verb_classes = args.nb_verb_classes
         noun_classes = 0
     else:
         raise NotImplementedError()
